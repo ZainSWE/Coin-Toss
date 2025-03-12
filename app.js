a6939cf1f678973e965c91c965d482a40b04094a
Zain Ahmad <165908202+ZainSWE@users.noreply.github.com>
2025-03-12 18:55:07 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-function button() {
+let heads = 0;
+let tails = 0;
+document.getElementById("score count").innerHTML = `Heads: ${heads} Tails: ${tails}`;
+
+function toss() {
   console.log('button was pressed');
   const video = document.getElementById('background');
   const audio = document.getElementById('coin');
+  
+  const randomNumber = Math.random();
+  console.log(randomNumber);
+
+  if (randomNumber > 0.5) {
+    console.log('heads');
+    heads += 1;
+    document.getElementById("score count").innerHTML = `Heads: ${heads} Tails: ${tails}`;
+
+  } else if (randomNumber < 0.5) {
+    console.log('tails');
+    tails += 1;
+    document.getElementById("score count").innerHTML = `Heads: ${heads} Tails: ${tails}`;
+  }
 
   if (video.paused) {
     video.currentTime = 0;
