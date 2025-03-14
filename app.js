7750a679a2df0418919f8a701a9c0eb6c7ad4f53
Zain Ahmad <165908202+ZainSWE@users.noreply.github.com>
2025-03-14 08:36:59 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,28 +1,53 @@
 let heads = 0;
 let tails = 0;
+let currentStreak = 0;
+let lastResult = '';
 document.getElementById("score count").innerHTML = `Heads: ${heads} Tails: ${tails}`;
 
 function toss() {
   console.log('button was pressed');
   const video = document.getElementById('background');
+  const streakVideo = document.getElementById('fiveXstreak');
   const audio = document.getElementById('coin');
   const button = document.getElementById('tossButton');
 
   button.disabled = true;
 
   const randomNumber = Math.random();
+  const headStreak = 0;
+  const tailStreak = 0;
   console.log(randomNumber);
 
   if (randomNumber > 0.5) {
     console.log('heads');
     heads += 1;
+
+    if (lastResult === 'heads') {
+      currentStreak++;
+    } else {
+      currentStreak = 1;
+    }
+
+    lastResult = 'heads';
+
     document.getElementById("score count").innerHTML = `Heads: ${heads} Tails: ${tails}`;
 
   } else if (randomNumber < 0.5) {
     console.log('tails');
     tails += 1;
+
+    if (lastResult === 'tails') {
+      currentStreak++
+    } else {
+      currentStreak = 1;
+    }
+
+    lastResult = 'tails';
+
     document.getElementById("score count").innerHTML = `Heads: ${heads} Tails: ${tails}`;
   }
+
+  console.log(`current streak: ${currentStreak}`);
 
   if (video.paused) {
     video.currentTime = 0;
@@ -38,7 +63,11 @@ function toss() {
     button.disabled = false;
   };
 
-  //multiplier();
+  streakCount();
+
+  if (button === 'resetScore') {
+    resetScore();
+  }
 }
 
 function customToss() {
@@ -49,19 +78,26 @@ function customToss() {
   }
 }
 
-/*
-function multiplier() {
+function streakCount() {
+  const streakVideo = document.getElementById('fiveXstreak');
 
-  for (let i = 0; i < 5; i++) {
-    if (heads === 1 && tails === 0) {
-      heads += 1 * 5;
-    }
-  }
+  if (currentStreak === 5) {
+    console.log('Streak Level 1 (5x)');
+    
+    streakVideo.style.display = 'block';
+    streakVideo.currentTime = 0;
+    streakVideo.play();
 
-  for (let i = 0; i < 5; i++) {
-    if (tails === 1 && heads === 0) {
-      tails += 1 * 5;
-    }
+    streakVideo.onended = () => {
+      streakVideo.style.display = 'none';
+    };
   }
 }
-*/
+
+function resetScore() {
+  heads = 0;
+  tails = 0;
+  currentStreak = 0;
+  console.log('SCORE RESET');
+  document.getElementById("score count").innerHTML = `Heads: ${heads} Tails: ${tails}`;
+}
